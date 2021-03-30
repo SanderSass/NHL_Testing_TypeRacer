37985db44b93eb5373f6f84852a25f242835da14
skitzke <egslotz@gmail.com>
2021-03-30 11:10:55 +0200

    Unit testing structure made

---
+++ WPM/src/com/practiseExam/MainTest.java
@@ -0,0 +1,18 @@
+package com.practiseExam;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void startCountdownTimer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWpm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getParagraph() {
+    }
+}
